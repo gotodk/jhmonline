4665fe7afa050e162fe4f9c5fc283ce0b357cf91
gotodk <gotodk@sina.com>
2017-05-03 09:33:06 +0800

    hhh

--- ws界面中心/cj/js/index.js
+++ ws界面中心/cj/js/index.js
@@ -1,5 +1,5 @@
 var t, //循环
-c=7,   //图片数量
+c=0,   //图片数量
 a,     //随机数
 b=7;   //结果数
 
@@ -25,15 +25,45 @@ function huoqu(){
  */
 function huoqu2() {
   
-    a = Math.floor(Math.random() * c + 1);
+    var jx = true;
+
+    while(jx)
+    {
+        
+        a = Math.floor(Math.random() * c + 1);
+        
+        if ($(".classtt_" + a).length < 1) {
+            jx = false;
+             
+        }
+        if ($(".img-chosed").length == 7) {
+            jx = false;
+
+        }
+
+    }
+    //a = Math.floor(Math.random() * c + 1);
     var src_tu = $("#cjzp_" + a).attr("src");
     $('#all-picture').html("<img src=\"" + src_tu + "\" id=\"img-sdf1\" width=\"100%\" height=\"100%\">");
      t=setTimeout("huoqu2()",50);
 }
 
-function start(){
-    $('#btn-kfj').html("<button id=\"button\" type=\"button\" onclick=\"stop()\" class=\"btn btn-danger btn-default\">点击结束</button>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <button type=\"button\" class=\"btn btn-primary\" onclick=\"reset()\">重置</button>");
-   huoqu2();
+function start() {
+
+
+
+  
+
+    //$(".btn-primary").text($(".img-chosed").length + "--" + c);
+    if ($(".img-chosed").length >= c) {
+        return;
+
+    }
+    else {
+        $('#btn-kfj').html("<button id=\"button\" type=\"button\" onclick=\"stop()\" class=\"btn btn-danger btn-default\">点击结束</button>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <button type=\"button\" class=\"btn btn-primary\" onclick=\"reset()\">重置</button>");
+        huoqu2();
+    }
+ 
 }
 
 function reset(){
@@ -45,10 +75,10 @@ function reset(){
 
 function stop(){
    clearTimeout(t);
-   var k = c + 1;
+ 
    var src_tu = $("#cjzp_" + a).attr("src");
    $('#box2').css("background", "url('" + src_tu + "') 50% 50%");
-   $('#is-picture').append("<img class='img-chosed' src=\"" + src_tu + "\" id=\"img-sdf1\" width=\"13%\" height=\"100%\" />");
+    $('#is-picture').append("<img class='img-chosed classtt_" + a + "' src=\"" + src_tu + "\" id=\"img-sdf1\" width=\"13%\" height=\"100%\" />");
    $('#is-picture_info').append("[" + $("#cjzp_" + a).attr("str_xingming") + "," + $("#cjzp_" + a).attr("str_xingbie") + "," + $("#cjzp_" + a).attr("str_dianhua") + "],");
    if(b>1){
        $('#btn-kfj').html("<button id=\"button\" type=\"button\" onclick=\"start()\" class=\"btn btn-success btn-default\">点击开始</button>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <button type=\"button\" class=\"btn btn-primary\" onclick=\"reset()\">重置</button>");
